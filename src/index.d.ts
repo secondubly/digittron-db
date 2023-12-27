78c2077c70a875d54305d101eb340083fad80a8a
secondubly <secondubly@gmail.com>
2023-12-27 13:34:03 -0500

    export prisma client

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-export { CommandType } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 export { Prisma } from ".prisma/client";
 
 export interface Context {
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { Context } from "./index.d";
 
 const prisma = new PrismaClient();
 
-export const createContext = async (): Promise<Context> => ({ prisma });
+export const createContext = async (): Promise<Context> => ({ prisma })
 export { $Enums } from '@prisma/client'
