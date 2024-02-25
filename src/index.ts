ad266f1ea0886b975086fe750b986086d4444c45
secondubly <secondubly@gmail.com>
2024-02-25 13:00:03 -0500

    load db from env variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { Context } from "./index.d";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL
+        }
+    }
+});
 
 export const createContext = async (): Promise<Context> => ({ prisma })
 export { $Enums, Prisma } from '@prisma/client'
