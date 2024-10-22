2589d871f7034a88552aaef19db1b18831f63922
secondubly <secondubly@gmail.com>
2024-10-21 18:15:40 -0700

    update package (swap to .js and cleanup)

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-export { Prisma } from ".prisma/client";
-
-export interface Context {
-  prisma: PrismaClient
-}
-
-export function createContext(): Promise<Context>;
