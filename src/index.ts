fbbf502be15035d280c67af3889ba9c5e7906136
secondubly <secondubly@gmail.com>
2025-06-20 14:20:22 -0700

    feat!:remove insert for permissions table

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,5 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { usersTable, permissionLevelsTable } from './db/schema.js';
+import * as schema from './db/schema.js';
 
-type Permission = typeof permissionLevelsTable.$inferInsert
-
-const db = drizzle(process.env.DATABASE_URL!)
-
-
-// TODO: add to init script
-const permissions: Permission[] = [
-    {
-        level: 0,
-        name: "BROADCASTER",
-    },
-    {
-        level: 1,
-        name: "MODERATOR",
-    },
-    {
-        level: 2,
-        name: "SUBSCRIBER"
-    },
-    {
-        level: 3,
-        name: "VIP"
-    },
-    {
-        level: 4,
-        name: "VIEWER"
-    }
-]
-
-await db.insert(permissionLevelsTable).values(permissions)
+const db = drizzle(process.env.DATABASE_URL!, { logger: true, schema })
